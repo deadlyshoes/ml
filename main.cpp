d79851c32291292a527a86d3a9a02197449e9981
deadlyshoes <khoseilmar@gmail.com>
2022-07-31 12:43:43 -0300

    update main

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ int main() {
     std::vector<int> nodes_per_hidden_layer = {
         {2}
     };
+    float learning_rate = 0.1;
 
-    NeuralNetwork nn = NeuralNetwork(input_size, nodes_per_hidden_layer);
+    NeuralNetwork nn = NeuralNetwork(input_size, nodes_per_hidden_layer, 0.1);
 
-    std::vector<std::vector>> X = {
+    std::vector<std::vector<float>> X = {
         {1, 1},
         {2, 2},
         {3, 3},
@@ -25,7 +26,19 @@ int main() {
     };
 
     std::vector<float> x = {1, 1};
+    std::vector<float> y = {2, 2};
 
+    std::cout << "Before training" << std::endl;
     float test_output = nn.predict(x);
-    std::cout << test_output << std::endl;
+    std::cout << "{1, 1}: " << test_output << std::endl;
+    test_output = nn.predict(y);
+    std::cout << "{2, 2}: " << test_output << std::endl;
+    
+    nn.train(X, Y);
+
+    std::cout << "After training" << std::endl;
+    test_output = nn.predict(x);
+    std::cout << "{1, 1}: " << test_output << std::endl;
+    test_output = nn.predict(y);
+    std::cout << "{2, 2}: " << test_output << std::endl;
 }
